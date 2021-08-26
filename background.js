f5776ae6728066e26a9001e50713cea361469de5
Vitaliy-Tymoshenko <v.tymoshenko.dev@gmail.com>
2021-08-26 10:52:58 +0300

    like work

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-let heightArtcl = 100;
+
 
 
 
--- popup.js
+++ popup.js
@@ -1,5 +1,30 @@
+let start = document.getElementById("start");
+
+start.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: main,
+  });
+});
 
 
+function main() {
+  let likes = document.getElementsByClassName('fr66n');
+
+  like(likes);
+  
+
+
+  function like (likes) {
+    let toLike = Object.values(likes).filter(el => el.childNodes[0].childNodes[0].childNodes[0].childNodes[0].ariaLabel == 'Like'); 
+  
+    toLike.forEach(el => {
+      el.childNodes[0].click();
+    })
+  }
+}
 
 
 
